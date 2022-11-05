fc677a273f5a8c13febaf18324626ffdbc70794b
Piriya <piriya.muaithaisong@gmail.com>
2022-11-05 22:31:31 +0700

    Add search input box

--- src/App.js
+++ src/App.js
@@ -7,14 +7,17 @@ class App extends Component {
   constructor() {
     super();
 
+    console.log('constructor')
     this.state = {
-      monsters : [],
-      tests:{tests:'1234'}
+      monsters : []
     };
   }
 
   // This life cycle run when the first time coponent reder on the page
   componentDidMount() {
+
+    console.log('componentDidMount')
+
     fetch('http://jsonplaceholder.typicode.com/users')
       .then((response) => response.json())
       .then((users) => this.setState(
@@ -28,8 +31,14 @@ class App extends Component {
   }
 
   render() {
+
+    console.log('render')
+
     return (
       <div className="App">
+        <input className='search-box' type='search' placeholder='search monsters' onChange={(event)=>{
+          console.log(event.target.value);
+        }}/>
        {
         this.state.monsters.map((monster) => {
           return (
