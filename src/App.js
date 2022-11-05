187c7aea5bfff98d5114ae17c0bb0f1291dfb936
Piriya <piriya.muaithaisong@gmail.com>
2022-11-05 21:08:22 +0700

    Map monsters

--- src/App.js
+++ src/App.js
@@ -8,31 +8,31 @@ class App extends Component {
     super();
 
     this.state = {
-      name: {firstName: "Piriya", lastName: "Muaithaisong"}
+      monsters : [
+        {
+          name:'Linda',
+          id : '123'
+        },
+        {
+          name:'Frank',
+          id : '456'
+        },
+        {
+          name:'Jacky',
+          id : '789'
+        }
+      ]
     }
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hi {this.state.name.firstName}  {this.state.name.lastName}  
-          </p>
-          <button onClick={()=>{
-            this.setState((state, props)=>{
-              return {
-                name: {firstName: "TEST", lastName: "TEST"}
-              }
-            },
-            ()=>{
-              console.log(this.state)
-            });
-          }}>
-            Change Name!
-          </button>
-        </header>
+       {
+        this.state.monsters.map((monster) => {
+          return <h1 key={monster.id}>{ monster.name }</h1>
+        })
+       }
       </div>
     );
   }
